74c3c4793d52ee8a1a6e4167c5037caf22e9c955
diego.mendez.medina <diegomm@ciencias.unam.mx>
2020-11-17 13:25:08 -0600

    Valid photo and flag

--- appy.py
+++ appy.py
@@ -9,11 +9,16 @@ def read_terminal():
     program = txt[0]
     photo = txt[1]
     flag =  "none"
-    if len (txt) >2:
+    if len (txt) > 2:
         flag = txt[2]
-    #valid_program()
+    #valid_program(program)
     v_photo = check_photo_existance(photo)
-    v_flag = valid_flag(flag)
+    v_flag = valid_flag(flag, v_photo)
+    
+    if v_photo:
+            print("function to get cloud coverage index, params: photo, v_flag")
+    else :
+        print("Photo not found on resources");
 
 def check_photo_existance(photo):
     fileName = r"resources/"+photo
@@ -21,7 +26,10 @@ def check_photo_existance(photo):
     return fileObj.is_file()
 
 
-def valid_flag(flag):
-    if flag.upper() == 'S':
-        return True
-    return False
+def valid_flag(flag, v_photo):
+    if flag.upper() != 'S':
+        print("Invalid flag")
+        if v_photo:
+            print("Program still running")
+        return False
+    return True
