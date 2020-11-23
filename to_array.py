a16285379a0a4463c5f2bd307b33f14285433d01
diego.mendez.medina <diegomm@ciencias.unam.mx>
2020-11-22 21:57:03 -0600

    array masked

--- to_array.py
+++ to_array.py
@@ -3,13 +3,39 @@ import numpy as np
 
 def img_to_array(photo, v_flag):
     ''' Changes the photo size, to the intrested area '''
+
+    photo_split = photo.split(".")
+    photo_cad = photo_split[0]
     
     im = np.array(Image.open('samples/'+photo))
     im_trim = im[106:2806, 825:3525]
-    cad = im_trim.shape
+    
+    mask = np.array(Image.open('samples/mask-1350-sq.png'))
+    mask = mask.reshape(*mask.shape, 1)
+    
+    mask = mask / 255
+
+
+    dst = im_trim * mask
+    
+    Image.fromarray(dst.astype(np.uint8)).save('samples/'+photo_cad+'.png')
+
+    im_mask = np.array(Image.open('samples/'+photo_cad+'.png'))
+    cad = im_mask.shape
     height = cad[0]
     width = cad[1]
+    im_mask = np.dstack( (dst, np.zeros((width, height))))
 
-    return im_trim
-    #Image.fromarray(im_trim).save('samples/output/save.jpg')
+    
+    #im_mask = im_mask.astype(np.uint8)
+
+    
+    for i in range(width):
+        for j in range(height):
+            R, G, B, A = im_mask[i,j]
+            if R+G+B > 0:
+                im_mask[i, j] = (R, G, B, 255)
+                
+    return im_mask
+    
     
