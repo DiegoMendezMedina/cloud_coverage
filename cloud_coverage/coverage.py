6880f6af495b26cfdb2f0ddce4e64a98f866e407
Luis <luis.onofre@ciencias.unam.mx>
2020-11-29 16:24:44 -0600

    Change the use of four layers instead of two for cloudiness_index function

--- cloud_coverage/coverage.py
+++ cloud_coverage/coverage.py
@@ -25,13 +25,13 @@ def cloudiness(image: np.ndarray) -> tuple:
     threshold = 0.95
     red_channel = image[:,:,0]
     blue_channel = image[:,:,2]
-    mask = image[:,:,3]
+
     parameters = {"out": np.zeros(red_channel.shape, dtype= float), "where": blue_channel != 0}
     ratios = np.divide(red_channel, blue_channel, **parameters)
-    segmentation = np.zeros_like(image[:,:,:2])
-    segmentation[:,:,0] = (ratios >= threshold) * 1 
-    segmentation[:,:,1] = mask 
-    return makepng(segmentation), cloudiness_index(segmentation) 
+    
+    segmentation = np.copy(image)
+    segmentation[:,:,0] = segmentation[:,:,1] = segmentation[:,:,2] = (ratios >= threshold) * 255 
+    return segmentation, cloudiness_index(segmentation) 
 
 def cloudiness_index(image: np.ndarray) -> float:
     """ Calculates the cloudiness index based on a segmented image
@@ -39,8 +39,8 @@ def cloudiness_index(image: np.ndarray) -> float:
     Parameters
     ----------
     image: numpy.ndarray
-        It's a third dimensional segmented array with the first layer the segmented image
-        with values 0 to 1 for each pixel and the second layer a mask, where the area will be
+        It's a third dimensional segmented array with the first three layers are the segmented image
+        with values 0 or 255 for each pixel and the second layer a mask, where the area will be
         calculated
 
     Returns
@@ -49,31 +49,12 @@ def cloudiness_index(image: np.ndarray) -> float:
         the cloudiness index, which goes from 0 to 1
     """
 
-    segmentation = image[:,:,0]
-    mask = image[:,:,1] == 255
-    cropped_image = segmentation[mask]
-    cloud_pixels = np.sum(cropped_image == 1)
+    segmented_layer = image[:,:,0]
+    mask = image[:,:,3] == 255
+    cropped_image = segmented_layer[mask]
+    if cropped_image.size == 0:
+        return 0
+
+    cloud_pixels = np.sum(cropped_image == 255)
     return cloud_pixels / cropped_image.size 
 
-def makepng(image: np.ndarray) -> np.ndarray:
-    """ Makes a png image array with a segmented image, each layer
-    will represent each RGBA channel
-
-    Parameters
-    ----------
-    image: numpy.ndarray
-        dd
-
-    Returns
-    -------
-    numy.ndarray
-        an array containing the RGBA channels of the image
-    """
-
-    width, height, _ = image.shape
-    png = np.zeros((width, height, 4), dtype = np.uint8)
-    # R,G,B channels
-    png[:,:,0] = png[:,:,1] = png[:,:,2] = image[:,:,0] * 255
-    # Alpha channel
-    png[:,:,3] = image[:,:,1]
-    return png
